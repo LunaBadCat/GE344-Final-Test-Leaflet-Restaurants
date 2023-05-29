6547288254cef4e4e00568e904e1827f470203d0
LunaBadCat <124545699+LunaBadCat@users.noreply.github.com>
2023-05-29 11:00:44 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,135 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "ผัดไทยประตูผี",
+      "ประเภท": "Street food",
+      "ระดับ": 10},
+      "geometry": {
+        "coordinates": [
+          100.50473980526624,
+          13.752805557494725
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "เจ๊ไฝ",
+      "ประเภท": "Street food",
+      "ระดับ": 10},
+      "geometry": {
+        "coordinates": [
+          100.50468911025172,
+          13.752567216435367
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "ข้าวซอยนิมมาน",
+      "ประเภท": "Noodle",
+      "ระดับ": 9},
+      "geometry": {
+        "coordinates": [
+          98.969287898284,
+          18.797977981027685
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "กุ้งอบภูเขาไฟ",
+      "ประเภท": "Thai food",
+      "ระดับ": 8},
+      "geometry": {
+        "coordinates": [
+          100.04130499999849,
+          13.804192837907493
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "Miki Cafe",
+      "ประเภท": "Cafe",
+      "ระดับ": 10},
+      "geometry": {
+        "coordinates": [
+          100.04327754232543,
+          13.814832000000692
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "ครัวเจ้าพระยา",
+      "ประเภท": "Thai food",
+      "ระดับ": 8},
+      "geometry": {
+        "coordinates": [
+          100.49567000000036,
+          13.878396415629823
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "เนื้อแท้",
+      "ประเภท": "Steak",
+      "ระดับ": 8},
+      "geometry": {
+        "coordinates": [
+          100.52853908465738,
+          13.733936266256606
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "Coffee War - 331 station",
+      "ประเภท": "Cafe",
+      "ระดับ": 9},
+      "geometry": {
+        "coordinates": [
+          100.99072399999955,
+          12.757077463995472
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "ย่างเนย",
+      "ประเภท": "Grill",
+      "ระดับ": 8},
+      "geometry": {
+        "coordinates": [
+          100.63941199999954,
+          13.768082682261777
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "ชื่อแหล่งท่องเที่ยว": "ใส่นม",
+      "ประเภท": "Cafe",
+      "ระดับ": 8},
+      "geometry": {
+        "coordinates": [
+          100.56186099999837,
+          13.813468837078517
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
